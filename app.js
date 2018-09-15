ac752afae6be75c034cd7228ffe1a43baa1400aa
あるどーす <argyros.friys@gmail.com>
2018-09-16 02:14:31 +0900

    app.jsからroute機能分離

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 //FIXME 各環境ごとに設定を切り替えられるようにする
+//---------------------- ライブラリ読込 ----------------------
 var express = require("express");
 var app = express();
 const connection = require("./lib/db/postgreSQLConnection");
+//---------------------- ライブラリ読込 ----------------------
 
 //---------------------- ミドルウェア構成 ----------------------
 //静的ファイル
@@ -14,57 +16,20 @@ app.use('/static/css', express.static(__dirname + '/node_modules/bootstrap-honok
 app.use('/static/image', express.static(__dirname + '/static/image'));
 //json
 app.use(require('body-parser')());
+//route
+app.use("/", require("./route/route"));
 //---------------------- ミドルウェア構成 ----------------------
 
 //---------------------- 各種の設定情報 ----------------------
 //post
 app.set('port', (process.env.PORT || 3000));
 //EJS
-app.set( 'views', __dirname + '/view' );
+app.set('views', __dirname + '/view');
 app.set('view engine', 'ejs');
 //---------------------- 各種の設定情報 ----------------------
 
 //---------------------- サーバー構築 ----------------------
-var server = app.listen(app.get('port'), function(){
+var server = app.listen(app.get('port'), function () {
     console.log("Node.js is listening to PORT:" + server.address().port);
 });
 //---------------------- サーバー構築 ----------------------
-
-app.get("/", function(req, res, next){
-    res.render("index/index", {});
-});
-
-//ログイン画面
-app.get("/login", function(req, res, next){
-    res.render("login/login", {});
-});
-
-app.post("/login/auth",function(req, res){
-
-    //FIXME ログインの認証機能についてはpassportを使用する
-    console.log(req.body.user);
-
-    // connection.query('select * from test')
-    // .then(result => {
-    //   console.log(result.rows[0])
-    //   connection.end()
-    // })
-    // .catch(e => {
-    //   console.error(e.stack)
-    //   connection.end()
-    // });
-
-    res.render("viewer/viewer", {});
-});
-
-//404NotFound
-app.use(function(req, res, next){
-	res.status(404);
-    res.render("error/notFound", {});
-});
-
-//500InternalServerError
-app.use(function(err, req, res, next){
-	res.status(500);
-	res.end('my 500 error! : ' + err);
-});
---
+++ route/route.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var router = express.Router();
+
+//ログイン画面
+router.get("/", function (req, res, next) {
+    res.redirect(302, "/login");
+});
+router.get("/login", function (req, res, next) {
+    res.render("login/login", {});
+});
+
+//ログイン認証
+router.post("/login/auth", function (req, res) {
+    //FIXME ログインの認証機能についてはpassportを使用する
+    console.log(req.body.user);
+    res.redirect(302, "/viewer");
+});
+
+//メインページ
+router.get("/viewer", function (req, res) {
+    res.render("viewer/viewer", {});
+});
+
+//404NotFound
+router.use(function (req, res, next) {
+    res.status(404);
+    res.render("error/notFound", {});
+});
+
+//500InternalServerError
+router.use(function (err, req, res, next) {
+    res.status(500);
+    res.end('my 500 error! : ' + err);
+});
+
+module.exports = router;
