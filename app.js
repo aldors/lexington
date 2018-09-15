01500c264a4c59b791aa1fe34bd940364a1ddd94
あるどーす <argyros.friys@gmail.com>
2018-09-15 15:20:21 +0900

    静的ファイルの配置場所見直し
    staticフォルダ配下を参照するようにする
    static/js
    static/css
    static/image
    etc...

--- app.js
+++ app.js
@@ -3,21 +3,32 @@ var express = require("express");
 var app = express();
 const connection = require("./lib/db/postgreSQLConnection");
 
-app.set('port', (process.env.PORT || 3000));
-
-//静的ファイル設定
-app.use(express.static(__dirname + '/static'));
-//post設定
+//---------------------- ミドルウェア構成 ----------------------
+//静的ファイル
+//※js,css等の静的ファイルは、static配下を参照するように設定すること
+app.use('/static/js', express.static(__dirname + '/static/js'));
+app.use('/static/js', express.static(__dirname + '/node_modules/bootstrap-honoka/dist/js'));
+app.use('/static/js', express.static(__dirname + '/node_modules/jquery/dist'));
+app.use('/static/css', express.static(__dirname + '/static/css'));
+app.use('/static/css', express.static(__dirname + '/node_modules/bootstrap-honoka/dist/css'));
+app.use('/static/image', express.static(__dirname + '/static/image'));
+//json
 app.use(require('body-parser')());
+//---------------------- ミドルウェア構成 ----------------------
 
-//EJS設定
+//---------------------- 各種の設定情報 ----------------------
+//post
+app.set('port', (process.env.PORT || 3000));
+//EJS
 app.set( 'views', __dirname + '/view' );
 app.set('view engine', 'ejs');
+//---------------------- 各種の設定情報 ----------------------
 
-//ポート設定
+//---------------------- サーバー構築 ----------------------
 var server = app.listen(app.get('port'), function(){
     console.log("Node.js is listening to PORT:" + server.address().port);
 });
+//---------------------- サーバー構築 ----------------------
 
 app.get("/", function(req, res, next){
     res.render("index/index", {});
