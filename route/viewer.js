ab9ed6a23f46cd74cb50ae43db89b252844bc3c7
あるどーす <argyros.friys@gmail.com>
2018-11-11 13:26:21 +0900

    viewer.js修正

--- route/viewer.js
+++ route/viewer.js
@@ -1,8 +1,21 @@
 var express = require('express');
 var router = express.Router();
 
+const connection = require("../lib/db/postgreSQLConnection");
+
 router.get("/viewer", function (req, res) {
-    res.render("viewer/viewer", {});
+    var sql = "select "
+        + "chunithm_music_mst.music_name,"
+        + "chunithm_music_mst.difficulty_level,"
+        + "chunithm_music_mst.music_constant,"
+        + "chunithm_play_record.score,"
+        + "chunithm_play_record.max_combo_count "
+        + "from chunithm_play_record "
+        + "inner join chunithm_music_mst on chunithm_play_record.chunithm_music_id = chunithm_music_mst.chunithm_music_id"
+
+    connection.select(sql, function (result) {
+        res.render("viewer/viewer", { recordList: result.rows });
+    });
 });
 
 module.exports = router;
